9f437f0cae71b35871d52a586063e07bbe670f9d
gaetan <gaetan.desrues@inria.fr>
2021-05-01 00:37:02 +0200

    update pypi package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="OarLauncher",
-    version="0.1.002",
+    version="0.1.003",
     author="Gaetan Desrues",
     author_email="gaetan.desrues@inria.fr",
     description="Description",
