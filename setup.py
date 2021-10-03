0489e9cd362e3fc81bff331e9435c7cc10cc1d59
gaetan <gaetan.desrues@inria.fr>
2021-10-04 00:02:54 +0200

    new release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="OarLauncher",
-    version="0.2.022",
+    version="0.2.023",
     author="Gaetan Desrues",
     author_email="gaetan.desrues@inria.fr",
     description="Description",
