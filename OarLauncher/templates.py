c679d6de2b53a8fc8136bd8b69c099562f55560e
gaetan <gaetan.desrues@inria.fr>
2021-10-04 00:02:44 +0200

    ADD: load cmake, boost, gcc modules

--- OarLauncher/templates.py
+++ OarLauncher/templates.py
@@ -9,6 +9,9 @@ RUNME_SCRIPT = """
 
 source /etc/profile.d/modules.sh
 module load conda/2020.11-python3.8
+module load cmake/3.19.6
+module load boost/1.58.0
+module load gcc/9.2.0
 
 source {activate}
 {python_path}
